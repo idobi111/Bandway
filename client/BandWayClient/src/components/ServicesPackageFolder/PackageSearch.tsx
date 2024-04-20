0f5faf2ce1407a2a0405144bc83ef6df162a0a77
Tal Yamin <talym@mta.ac.il>
2024-04-21 02:42:38 +0300

    Added City search mock to service package search box

--- client/BandWayClient/src/components/ServicesPackageFolder/PackageSearch.tsx
+++ client/BandWayClient/src/components/ServicesPackageFolder/PackageSearch.tsx
@@ -1,47 +1,39 @@
-import React, { useState } from 'react';
-import { Typography, Grid, TextField } from '@mui/material';
-import { HomeSearchGrid, SearchTextField, WindowDiv, ActionButton } from '../../styles/ComponentsStyles';
-import { useNavigate } from "react-router-dom";
+import React from 'react';
+import { Typography, Grid } from '@mui/material';
+import { HomeSearchGrid, WindowDiv, ActionButton } from '../../styles/ComponentsStyles';
+import DatePickerPopover from './DatePickerPopover';
 import OccupancyPopover from './OccupancyPopover';
 import ServicesBudgetPopover from './ServicesBudgetPopover';
-import DatePickerPopover from './DatePickerPopover';
-
+import CitySelect from './CitySelect';
+import { City } from '../../models/City';
+import { CityOption } from '../../models/CityOption';
 
 const PackageSearch: React.FC = () => {
-    // const [selectedDate, setSelectedDate] = useState<Date>();
+  const handleSelectCity = (city: CityOption) => {
+    console.log('Selected city:', city);
+  };
 
-    // const handleDateSelection = (newValue: Date) => {
-    //     setSelectedDate(newValue);
-    // };
-
-
-    return (
-        <WindowDiv>
-            <HomeSearchGrid container spacing={2} justifyContent="center" >
-                <Grid item xs={2}>
-                   <DatePickerPopover></DatePickerPopover>
-                </Grid>
-                <Grid item xs={2}>
-                    <OccupancyPopover></OccupancyPopover>
-                </Grid>
-                <Grid item xs={2}>
-                    <ServicesBudgetPopover></ServicesBudgetPopover>
-                </Grid>
-                <Grid item xs={2}>
-                    <Typography>
-                        Place
-                    </Typography>
-                    <SearchTextField style={{ width: '150px' }} placeholder="Event City" variant="standard" InputLabelProps={{
-                        style: { color: 'gray' },
-                        shrink: false
-                    }} />
-                </Grid>
-                <Grid item xs={2} sx={{ marginLeft: '40px' }}>
-                    <ActionButton variant='contained'>Search</ActionButton>
-                </Grid>
-            </HomeSearchGrid>
-        </WindowDiv>
-    );
+  return (
+    <WindowDiv>
+      <HomeSearchGrid container spacing={2} justifyContent="center">
+        <Grid item xs={2}>
+          <DatePickerPopover />
+        </Grid>
+        <Grid item xs={2}>
+          <OccupancyPopover />
+        </Grid>
+        <Grid item xs={2}>
+          <ServicesBudgetPopover />
+        </Grid>
+        <Grid item xs={2}>
+          <CitySelect onSelect={handleSelectCity} />
+        </Grid>
+        <Grid item xs={2} sx={{ marginLeft: '40px' }}>
+          <ActionButton variant='contained'>Search</ActionButton>
+        </Grid>
+      </HomeSearchGrid>
+    </WindowDiv>
+  );
 };
 
 export default PackageSearch;
