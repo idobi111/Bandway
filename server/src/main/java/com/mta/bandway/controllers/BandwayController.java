f5030bd58d38d2e2bd1c385173e5bbd4819561a9
idobi <idobi@mta.ac.il>
2024-04-23 23:40:28 +0300

    fix bug and adding testResponse files

--- server/src/main/java/com/mta/bandway/controllers/BandwayController.java
+++ server/src/main/java/com/mta/bandway/controllers/BandwayController.java
@@ -14,6 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @RestController
@@ -64,5 +65,10 @@ public class BandwayController {
     @PostMapping("/searchCarRental")
     public ResponseEntity<?> searchCar(@RequestBody CarRentalRequestDto requestCarRentalDto) {
         return ResponseEntity.ok(carRentalService.searchCarRental(requestCarRentalDto));
+    }
+
+    @GetMapping("/getHotelLink")
+    public ResponseEntity<?> getHotelLink(@RequestParam Integer hotelId, @RequestParam String checkInDate, @RequestParam String checkOutDate) {
+        return ResponseEntity.ok(hotelService.getLink(hotelId, checkInDate, checkOutDate));
     }
 }
--- server/src/main/java/com/mta/bandway/core/domain/hotel/HotelData.java
+++ server/src/main/java/com/mta/bandway/core/domain/hotel/HotelData.java
@@ -2,6 +2,6 @@ package com.mta.bandway.core.domain.hotel;
 
 import java.util.ArrayList;
 @lombok.Data
-public class Data{
+public class HotelData {
     private ArrayList<Hotel> hotels;
 }
