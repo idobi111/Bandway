4747268ab1dcb7c43fa9fac8a38ca24746c3c29d
Tal Yamin <talym@mta.ac.il>
2024-04-26 18:15:05 +0300

    Added flight details to package dialog window

---
+++ client/BandWayClient/src/components/ServicesPackageFolder/PackageCard/PackageDialog/PackageDialogFlightSection.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import { Typography, Stack, Box, Tooltip, Modal } from '@mui/material';
+import { Package } from '../../../../models/Package';
+import StarIcon from '@mui/icons-material/Star';
+import { PackageFilter } from '../../../../models/PackageFilter';
+import { ActionButton } from '../../../../styles/ComponentsStyles';
+import { HotelApi } from '../../../../apis/HotelApi';
+import FlightDetailsGrid from './FlightDetailsGrid';
+import { FlightService } from '../../../../services/FlightService';
+
+
+interface Props {
+    servicesPackage: Package | null;
+}
+
+const PackageDialogFlightSection: React.FC<Props> = ({ servicesPackage }) => {
+
+    const hotelApi = new HotelApi();
+    const flightService = new FlightService();
+
+    const handleSeeFlightAvailability = async () => {
+        // try {
+        //     const response = await hotelApi.getHotelLink(servicesPackage?.hotel?.hotelId);
+        //     const hotelUrl = response.body;
+
+        //     window.open("https://www.booking.com/hotel/ie/the-devlin.html?checkin=2024-05-25&checkout=2024-05-28", '_blank');
+        // } catch (error) {
+        //     console.error('Error fetching hotel URL:', error);
+        // }
+    };
+
+    return (
+        <>
+            <Stack display={'flex'} sx={{ p: 4 }}>
+                <Typography variant='h4'>Your Flights:</Typography>
+                <Typography variant='h6'> {servicesPackage && flightService.getFlightType(servicesPackage)}</Typography>
+                {servicesPackage?.flight?.departFlightDetails.map((departDetail, index) => (
+                    departDetail.flightDetails.map((flightDetail, flightIndex) => (
+                        <Box key={`${index}-${flightIndex}`} sx={{ mb: 2 }}>
+                            <FlightDetailsGrid
+                                flightDetails={flightDetail}
+                                marketing={departDetail.marketing[flightIndex]} 
+                            />
+                        </Box>
+                    ))
+                ))}
+                 <Typography variant='h6'> Start from ${servicesPackage?.flight?.departFlightDetails[0].price} per person</Typography>
+                {/* Add onClick event to trigger API request */}
+                <ActionButton
+                    variant="contained"
+                    color="primary"
+                    sx={{ width: '300px', fontSize: '20px' }}
+                    onClick={handleSeeFlightAvailability} 
+                >
+                    See flight availability
+                </ActionButton>
+            </Stack>
+        </>
+    );
+};
+
+export default PackageDialogFlightSection;
---
+++ client/BandWayClient/src/components/ServicesPackageFolder/PackageCard/PackageDialog/PackageDialogHotelSection.tsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Typography, Stack, Box, Tooltip, Modal } from '@mui/material';
+import { Package } from '../../../../models/Package';
+import StarIcon from '@mui/icons-material/Star';
+import { PackageFilter } from '../../../../models/PackageFilter';
+import { ActionButton } from '../../../../styles/ComponentsStyles';
+import { HotelApi } from '../../../../apis/HotelApi';
+
+
+interface Props {
+    servicesPackage: Package | null;
+}
+
+const PackageDialogHotelSection: React.FC<Props> = ({ servicesPackage }) => {
+
+    const hotelApi = new HotelApi();
+
+    const handleSeeHotelAvailability = async () => {
+        try {
+            const response = await hotelApi.getHotelLink(servicesPackage?.hotel?.hotelId);
+            const hotelUrl = response.body;
+
+            window.open("https://www.booking.com/hotel/ie/the-devlin.html?checkin=2024-05-25&checkout=2024-05-28", '_blank');
+        } catch (error) {
+            console.error('Error fetching hotel URL:', error);
+        }
+    };
+
+    return (
+        <>
+            <Stack display={'flex'} sx={{ p: 4 }}>
+                <Typography variant='h4'>Your Hotel:</Typography>
+                <Typography variant='h5'>{servicesPackage?.hotel.hotelName}</Typography>
+                <Typography variant='h6'>
+                    {servicesPackage && [...Array(Math.round(servicesPackage.hotel.rating / 2))].map((_, index) => (
+                        <StarIcon key={index} />
+                    ))}
+                </Typography>
+                <Typography variant='h6' color='text.secondary'>
+                    Rating: {servicesPackage?.hotel.rating}
+                </Typography>
+                <Typography variant='h6' color='text.secondary'>adults: {servicesPackage?.hotel.adults}, children: {servicesPackage?.hotel.children} </Typography>
+                <Typography variant='h6' color='text.secondary'>rooms: {servicesPackage?.hotel.rooms} </Typography>
+                <Typography variant='h6'> Start from ${servicesPackage?.hotel.price} per person</Typography>
+                {/* Add onClick event to trigger API request */}
+                <ActionButton
+                    variant="contained"
+                    color="primary"
+                    sx={{ width: '300px', fontSize: '20px' }}
+                    onClick={handleSeeHotelAvailability} // Call handleSeeHotelAvailability function
+                >
+                    See hotel availability
+                </ActionButton>
+            </Stack>
+
+        </>
+    );
+};
+
+export default PackageDialogHotelSection;
