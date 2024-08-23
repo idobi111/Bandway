455cf3f5659772d55184dd386ed8b49737a39f8f
idobi <idobi@mta.ac.il>
2024-08-23 12:45:04 +0300

    fix Groups object

--- server/src/main/java/com/mta/bandway/core/domain/car/Groups.java
+++ server/src/main/java/com/mta/bandway/core/domain/car/Groups.java
@@ -1,5 +1,8 @@
 package com.mta.bandway.core.domain.car;
 
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -7,7 +10,13 @@ public class Groups {
 
     private final Map<String, CarMetadata> groups = new HashMap<>();
 
+    @JsonAnyGetter
     public Map<String, CarMetadata> getGroups() {
         return groups;
     }
+    @JsonAnySetter
+    public void setGroups(String key, CarMetadata value) {
+        groups.put(key, value);
+    }
+
 }
